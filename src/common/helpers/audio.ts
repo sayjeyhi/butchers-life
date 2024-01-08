257860356ad2787bde2be34a3ade4955c62aed1d
sayjeyhi <sayjeyhi@gmail.com>
2024-01-08 23:09:29 +0100

    fix: audio play

--- src/common/helpers/audio.ts
+++ src/common/helpers/audio.ts
@@ -1,6 +1,10 @@
 export const collectAudio = new Audio('/audios/interface-124464.mp3');
 export const hitObstacleAudio = new Audio('/audios/grunt2-85989.mp3');
 
-export const playAudio = (instance: HTMLAudioElement) => {
+type Options = {
+  currentTime: number;
+};
+export const playAudio = (instance: HTMLAudioElement, { currentTime = 0 }: Options) => {
+  instance.currentTime = currentTime;
   instance.play();
 };
