ad213a6bee3bc38e00e36df8af3575c18a2ceff3
sayjeyhi <sayjeyhi@gmail.com>
2024-01-08 22:46:54 +0100

    fix: start game issue

--- src/pages/Play.tsx
+++ src/pages/Play.tsx
@@ -1,7 +1,15 @@
 import { GameCanvas } from '../game/GameCanvas.tsx';
 import { InGameOverlay } from '../game/InGameOverlay.tsx';
+import { useEffect } from 'react';
+import { useSetAtom } from 'jotai';
+import { startGameAtom } from '../atoms/game.ts';
 
 export function Play() {
+  const startGame = useSetAtom(startGameAtom);
+  useEffect(() => {
+    startGame();
+  }, [startGame]);
+
   return (
     <>
       <InGameOverlay />
