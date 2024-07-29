f7034266da2ee1a019e5ef97eaf9faa6616d5cbd
sayjeyhi <sayjeyhi@gmail.com>
2024-07-29 10:25:11 +0200

    fix: home page spacing

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -26,10 +26,10 @@ export function Home() {
       <img
         src="/images/butcher.png"
         alt="Logo"
-        className="relative mx-auto w-3/6 -translate-y-24 rounded-2xl bg-[rgba(255,255,255,0.2)] p-5 backdrop-blur md:w-2/6 lg:w-1/6"
+        className="relative mx-auto w-3/6 rounded-2xl bg-[rgba(255,255,255,0.2)] p-5 backdrop-blur md:w-2/6 lg:w-1/6"
       />
 
-      <h1 className="three-d-text -mt-20 mb-12 w-full text-center text-2xl font-bold md:text-4xl lg:text-5xl">
+      <h1 className="three-d-text mb-12 w-full text-center text-2xl font-bold md:text-4xl lg:text-5xl">
         Butcher's life
       </h1>
       <p className="mt-3 text-center text-xl">Run from Ghosts and Score some stars!</p>
